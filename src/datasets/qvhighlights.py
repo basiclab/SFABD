97ad22cfad59c3a00604d5f907aee5eb89ff909f
Yi-Lun Wu <w86763777@gmail.com>
2022-11-30 19:23:15 +0800

    add qvhighlights1s

--- src/datasets/qvhighlights.py
+++ src/datasets/qvhighlights.py
@@ -10,14 +10,18 @@ from src.datasets.base import CollateBase
 class QVHighlights(CollateBase):
     def __init__(
         self,
-        ann_file,           # path to annotation file (.json)
-        feat_dirs=[
+        ann_file,               # path to annotation file (.json)
+        feat_dirs,              # path to feature directories
+        fallback_feat_dir=[
             './data/QVHighlights/features/clip_features/',
             './data/QVHighlights/features/slowfast_features/',
-        ],                  # path to feature directories
+        ],                      # path to fallback feature directory
     ):
         super().__init__(ann_file)
         self.feat_dirs = feat_dirs
+        self.fallback_feat_dir = fallback_feat_dir
+
+        assert len(self.feat_dirs) == len(self.fallback_feat_dir)
 
     def get_feat_dim(self):
         return 2816
@@ -26,8 +30,14 @@ class QVHighlights(CollateBase):
     def get_feat(self, anno):
         feats = []
         min_len = 1000000
-        for dir in self.feat_dirs:
-            x = np.load(os.path.join(dir, anno['vid'] + '.npz'))['features']
+        for dir, fallback_dir in zip(self.feat_dirs, self.fallback_feat_dir):
+            path = os.path.join(dir, anno['vid'] + '.npz')
+            if os.path.exists(path):
+                x = np.load(path)['features']
+            else:
+                path = os.path.join(fallback_dir, anno['vid'] + '.npz')
+                assert os.path.exists(path), f"fallback path {path} doest not exist"
+                x = np.load(path)['features']
             x = torch.from_numpy(x).float()
             x = F.normalize(x, dim=1)
             feats.append(x)
@@ -37,16 +47,53 @@ class QVHighlights(CollateBase):
         return feats
 
 
-class QVHighlightsTrain(QVHighlights):
+class QVHighlights2s(QVHighlights):
+    def __init__(self, ann_file):
+        super().__init__(
+            ann_file,
+            feat_dirs=[
+                './data/QVHighlights/features/clip_features/',
+                './data/QVHighlights/features/slowfast_features/',
+            ]
+        )
+
+
+class QVHighlightsTrain2s(QVHighlights2s):
     def __init__(self):
         super().__init__(ann_file="./data/QVHighlights/train.json")
 
 
-class QVHighlightsVal(QVHighlights):
+class QVHighlightsVal2s(QVHighlights2s):
     def __init__(self):
         super().__init__(ann_file="./data/QVHighlights/val.json")
 
 
-class QVHighlightsTest(QVHighlights):
+class QVHighlightsTest2s(QVHighlights2s):
+    def __init__(self):
+        super().__init__(ann_file="./data/QVHighlights/test.json")
+
+
+class QVHighlights1s(QVHighlights):
+    def __init__(self, ann_file):
+        super().__init__(
+            ann_file,
+            feat_dirs=[
+                './data/QVHighlights/features_1s/clip_features/',
+                './data/QVHighlights/features_1s/slowfast_features/',
+            ]
+        )
+
+
+class QVHighlightsTrain1s(QVHighlights1s):
+    def __init__(self):
+        super().__init__(ann_file="./data/QVHighlights/train.json")
+
+
+class QVHighlightsVal1s(QVHighlights1s):
+    def __init__(self):
+        super().__init__(ann_file="./data/QVHighlights/val.json")
+
+
+class QVHighlightsTest1s(QVHighlights1s):
     def __init__(self):
         super().__init__(ann_file="./data/QVHighlights/test.json")
